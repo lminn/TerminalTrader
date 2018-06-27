bfcf5a340a4d28d42002465308bd3e5991f8b02f
Lindsay <lindsayminnock@gmail.com>
2018-06-27 16:05:35 -0400

    Added buy functionality

--- run/core/controllers/buy.py
+++ run/core/controllers/buy.py
@@ -7,10 +7,14 @@ from core import model
 
 controller = Blueprint('buy',__name__,url_prefix='/buy')
 
-@controller.route('/',methods=['GET'])
+@controller.route('/',methods=['GET','POST'])
 def show_buy():
-	if request.method == 'GET':
-		return render_template('buy.html')
+	if request.method == 'POST':
+		ticker_symbol = request.form["ticker"]
+		volume = request.form["volume"]
+		model.buy(ticker_symbol, volume, 'lindsay@aol.com')
+		print("Got some results!")
+		return render_template('buy.html', volume=volume, ticker_symbol=ticker_symbol)
 	else:
 		return render_template('buy.html')
 
